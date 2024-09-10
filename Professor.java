81e08048dda0a378ee874597a15daddd71447218
Denilson Santos <seconvida@gmail.com>
2024-09-10 00:00:19 -0300

    Add files via upload

---
+++ Professor.java
@@ -0,0 +1,16 @@
+public class Professor extends {
+    private String departamento;
+
+    public Professor(String Denilson, String CPF1234567890, String departamento) {
+        super(Denilson, CPF1234567890);
+        this.departamento = departamento;
+    }
+    // Getters e Setters
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        
+
+}
